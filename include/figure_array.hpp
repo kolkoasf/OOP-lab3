f8f8c6659991d1f707b41b8a153b12dddb6642dc
kolkoasf <kolko1nafig1Kaktyc@gmail.com>
2025-10-24 20:48:28 +0000

    lab3

---
+++ include/figure_array.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+#include "figure.hpp"
+
+namespace geometry {
+
+class FigureArray {
+ public:
+  FigureArray() = default;
+  ~FigureArray();
+
+  FigureArray(const FigureArray&) = delete;
+  FigureArray& operator=(const FigureArray&) = delete;
+
+  FigureArray(FigureArray&&) noexcept;
+  FigureArray& operator=(FigureArray&&) noexcept;
+
+  void Add(Figure* figure);
+  void Remove(size_t i);
+
+  size_t Size() const;
+  Figure* Get(size_t i) const;
+
+  double GetTotalArea() const;
+
+  bool operator==(const FigureArray& other) const;
+  bool operator!=(const FigureArray& other) const;
+
+  void PrintAll(std::ostream& os) const;
+
+  friend std::ostream& operator<<(std::ostream& os, const FigureArray& arr);
+
+ private:
+  std::vector<Figure*> figures_;
+};
+
+}  // namespace geometry
